a5f3f203ae3da32eb8afbfc0e88cdec1c22d153a
Francois Wautier <francois@wautier.eu>
2018-11-08 14:56:42 +0700

    This was not working. Still for Python 2. Made it into a simplistic interactive command. Tested with VStarcam

--- examples/continuous_move.py
+++ examples/continuous_move.py
@@ -1,63 +1,100 @@
-from time import sleep
-
+import asyncio, sys
 from onvif import ONVIFCamera
+
+IP="192.168.0.100"   # Camera IP address
+PORT=10080           # Port
+USER="admin"         # Username
+PASS="password"        # Password
+
 
 XMAX = 1
 XMIN = -1
 YMAX = 1
 YMIN = -1
+moverequest = None
+ptz = None
+active = False
 
-def perform_move(ptz, request, timeout):
+def do_move(ptz, request):
     # Start continuous move
+    global active
+    if active:
+        ptz.Stop({'ProfileToken': request.ProfileToken})
+    active = True
     ptz.ContinuousMove(request)
-    # Wait a certain time
-    sleep(timeout)
-    # Stop continuous move
-    ptz.Stop({'ProfileToken': request.ProfileToken})
 
-def move_up(ptz, request, timeout=1):
-    print 'move up...'
-    request.Velocity.PanTilt._x = 0
-    request.Velocity.PanTilt._y = YMAX
-    perform_move(ptz, request, timeout)
+def move_up(ptz, request):
+    print ('move up...')
+    request.Velocity.PanTilt.x = 0
+    request.Velocity.PanTilt.y = YMAX
+    do_move(ptz, request)
 
-def move_down(ptz, request, timeout=1):
-    print 'move down...'
-    request.Velocity.PanTilt._x = 0
-    request.Velocity.PanTilt._y = YMIN
-    perform_move(ptz, request, timeout)
+def move_down(ptz, request):
+    print ('move down...')
+    request.Velocity.PanTilt.x = 0
+    request.Velocity.PanTilt.y = YMIN
+    do_move(ptz, request)
 
-def move_right(ptz, request, timeout=1):
-    print 'move right...'
-    request.Velocity.PanTilt._x = XMAX
-    request.Velocity.PanTilt._y = 0
-    perform_move(ptz, request, timeout)
+def move_right(ptz, request):
+    print ('move right...')
+    request.Velocity.PanTilt.x = XMAX
+    request.Velocity.PanTilt.y = 0
+    do_move(ptz, request)
 
-def move_left(ptz, request, timeout=1):
-    print 'move left...'
-    request.Velocity.PanTilt._x = XMIN
-    request.Velocity.PanTilt._y = 0
-    perform_move(ptz, request, timeout)
+def move_left(ptz, request):
+    print ('move left...')
+    request.Velocity.PanTilt.x = XMIN
+    request.Velocity.PanTilt.y = 0
+    do_move(ptz, request)
+    
 
-def continuous_move():
-    mycam = ONVIFCamera('192.168.0.112', 80, 'admin', '12345')
+def move_upleft(ptz, request):
+    print ('move up left...')
+    request.Velocity.PanTilt.x = XMIN
+    request.Velocity.PanTilt.y = YMAX
+    do_move(ptz, request)
+    
+def move_upright(ptz, request):
+    print ('move up left...')
+    request.Velocity.PanTilt.x = XMAX
+    request.Velocity.PanTilt.y = YMAX
+    do_move(ptz, request)
+    
+def move_downleft(ptz, request):
+    print ('move down left...')
+    request.Velocity.PanTilt.x = XMIN
+    request.Velocity.PanTilt.y = YMIN
+    do_move(ptz, request)
+    
+def move_downright(ptz, request):
+    print ('move down left...')
+    request.Velocity.PanTilt.x = XMAX
+    request.Velocity.PanTilt.y = YMIN
+    do_move(ptz, request)
+
+def setup_move():
+    mycam = ONVIFCamera(IP, PORT, USER, PASS)
     # Create media service object
     media = mycam.create_media_service()
+    
     # Create ptz service object
+    global ptz
     ptz = mycam.create_ptz_service()
 
     # Get target profile
-    media_profile = media.GetProfiles()[0];
+    media_profile = media.GetProfiles()[0]
 
     # Get PTZ configuration options for getting continuous move range
     request = ptz.create_type('GetConfigurationOptions')
-    request.ConfigurationToken = media_profile.PTZConfiguration._token
+    request.ConfigurationToken = media_profile.PTZConfiguration.token
     ptz_configuration_options = ptz.GetConfigurationOptions(request)
 
-    request = ptz.create_type('ContinuousMove')
-    request.ProfileToken = media_profile._token
+    global moverequest
+    moverequest = ptz.create_type('ContinuousMove')
+    moverequest.ProfileToken = media_profile.token
+    if moverequest.Velocity is None:
+        moverequest.Velocity = ptz.GetStatus({'ProfileToken': media_profile.token}).Position
 
-    ptz.Stop({'ProfileToken': media_profile._token})
 
     # Get range of pan and tilt
     # NOTE: X and Y are velocity vector
@@ -67,17 +104,53 @@ def continuous_move():
     YMAX = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].YRange.Max
     YMIN = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].YRange.Min
 
-    # move right
-    move_right(ptz, request)
 
-    # move left
-    move_left(ptz, request)
-
-    # Move up
-    move_up(ptz, request)
-
-    # move down
-    move_down(ptz, request)
-
+def readin():
+    """Reading from stdin and displaying menu"""
+    global moverequest, ptz
+    
+    selection = sys.stdin.readline().strip("\n")
+    lov=[ x for x in selection.split(" ") if x != ""]
+    if lov:
+        
+        if lov[0].lower() in ["u","up"]:
+            move_up(ptz,moverequest)
+        elif lov[0].lower() in ["d","do","dow","down"]:
+            move_down(ptz,moverequest)
+        elif lov[0].lower() in ["l","le","lef","left"]:
+            move_left(ptz,moverequest)
+        elif lov[0].lower() in ["l","le","lef","left"]:
+            move_left(ptz,moverequest)
+        elif lov[0].lower() in ["r","ri","rig","righ","right"]:
+            move_right(ptz,moverequest)
+        elif lov[0].lower() in ["ul"]:
+            move_upleft(ptz,moverequest)
+        elif lov[0].lower() in ["ur"]:
+            move_upright(ptz,moverequest)
+        elif lov[0].lower() in ["dl"]:
+            move_downleft(ptz,moverequest)
+        elif lov[0].lower() in ["dr"]:
+            move_downright(ptz,moverequest)
+        elif lov[0].lower() in ["s","st","sto","stop"]:
+            ptz.Stop({'ProfileToken': moverequest.ProfileToken})
+            active = False
+        else:
+            print("What are you asking?\tI only know, 'up','down','left','right', 'ul' (up left), \n\t\t\t'ur' (up right), 'dl' (down left), 'dr' (down right) and 'stop'")
+         
+    print("")
+    print("Your command: ", end='',flush=True)
+       
+            
 if __name__ == '__main__':
-    continuous_move()
+    setup_move()
+    loop = asyncio.get_event_loop()
+    try:
+        loop.add_reader(sys.stdin,readin)
+        print("Use Ctrl-C to quit")
+        print("Your command: ", end='',flush=True)
+        loop.run_forever()
+    except:
+        pass
+    finally:
+        loop.remove_reader(sys.stdin)
+        loop.close()
