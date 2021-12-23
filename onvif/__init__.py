7399aab6b5c73cebe4b0483088d883590a59a2ad
Andrey Martyanov <andrey@martyanov.com>
2021-12-23 13:58:36 +0600

    Provide some zeep exceptions to be available in userspace

--- onvif/__init__.py
+++ onvif/__init__.py
@@ -1,4 +1,6 @@
 from .client import ONVIFCamera  # noqa: F401
 from .client import ONVIFService  # noqa: F401
+from .exceptions import Fault  # noqa: F401
 from .exceptions import ONVIFError  # noqa: F401
+from .exceptions import ValidationError  # noqa: F401
 from .wsdl import SERVICES  # noqa: F401
--- onvif/exceptions.py
+++ onvif/exceptions.py
@@ -1,2 +1,6 @@
+from zeep.exceptions import Fault  # noqa: F401
+from zeep.exceptions import ValidationError  # noqa: F401
+
+
 class ONVIFError(Exception):
     """Base type for all ONVIF errors."""
