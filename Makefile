76288d31ed365aeea9a9a057851d0cc73987c7e5
Andrey Martyanov <andrey@martyanov.com>
2022-12-05 20:21:28 +0600

    Update specs from upstream and add media2 support

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 .DEFAULT: help
-.PHONY: help bootstrap build lint outdated test testcov testreport upload clean
+.PHONY: help bootstrap build lint outdated test testcov testreport upload syncspecs clean
 
 VENV = .venv
 PYTHON_BIN ?= python3
 PYTHON = $(VENV)/bin/$(PYTHON_BIN)
+SPEC_VERSION=48efdd21bc14de16d45ecdda0912e44a3e0086f9
 
 help:
 	@echo "Please use \`$(MAKE) <target>' where <target> is one of the following:"
@@ -16,6 +17,7 @@ help:
 	@echo "  testcov    - run project tests with coverage file report"
 	@echo "  testreport - run project tests and open HTML coverage report"
 	@echo "  upload     - upload built packages to package repository"
+	@echo "  syncspecs  - sync ONVIF specs from upstream repository"
 	@echo "  clean      - clean up project environment and all the build artifacts"
 
 bootstrap: $(VENV)/bin/activate
@@ -46,5 +48,13 @@ testreport: bootstrap
 upload: build
 	$(PYTHON) -m twine upload dist/*
 
+syncspecs:
+	mkdir -p .specs
+	wget -P .specs https://github.com/onvif/specs/archive/${SPEC_VERSION}.zip
+	unzip -f .specs/${SPEC_VERSION}.zip -d .specs
+	rm -rf aonvif/wsdl/ver10 aonvif/wsdl/ver20
+	cp -r .specs/specs-${SPEC_VERSION}/wsdl/ver10/ aonvif/wsdl/
+	cp -r .specs/specs-${SPEC_VERSION}/wsdl/ver20/ aonvif/wsdl/
+
 clean:
-	rm -rf *.egg-info .eggs .pytest_cache coverage.xml build dist htmlcov $(VENV)
+	rm -rf *.egg-info .eggs .pytest_cache .specs coverage.xml build dist htmlcov $(VENV)
--- aonvif/wsdl/__init__.py
+++ aonvif/wsdl/__init__.py
@@ -5,72 +5,77 @@ WSDL_DIR = pathlib.Path(__file__).parent
 SERVICES = {
     'devicemgmt': {
         'ns': 'http://www.onvif.org/ver10/device/wsdl',
-        'wsdl': 'devicemgmt.wsdl',
+        'wsdl': 'ver10/device/wsdl/devicemgmt.wsdl',
         'binding': 'DeviceBinding',
     },
     'media': {
         'ns': 'http://www.onvif.org/ver10/media/wsdl',
-        'wsdl': 'media.wsdl',
+        'wsdl': 'ver10/media/wsdl/media.wsdl',
         'binding': 'MediaBinding',
+    },
+    'media2': {
+        'ns': 'http://www.onvif.org/ver20/media/wsdl',
+        'wsdl': 'ver20/media/wsdl/media.wsdl',
+        'binding': 'Media2Binding',
     },
     'ptz': {
         'ns': 'http://www.onvif.org/ver20/ptz/wsdl',
-        'wsdl': 'ptz.wsdl',
+        'wsdl': 'ver20/ptz/wsdl/ptz.wsdl',
         'binding': 'PTZBinding',
     },
     'imaging': {
         'ns': 'http://www.onvif.org/ver20/imaging/wsdl',
-        'wsdl': 'imaging.wsdl',
+        'wsdl': 'ver20/imaging/wsdl/imaging.wsdl',
         'binding': 'ImagingBinding',
     },
     'deviceio': {
         'ns': 'http://www.onvif.org/ver10/deviceIO/wsdl',
-        'wsdl': 'deviceio.wsdl',
+        'wsdl': 'ver10/deviceio.wsdl',
         'binding': 'DeviceIOBinding',
     },
     'events': {
         'ns': 'http://www.onvif.org/ver10/events/wsdl',
-        'wsdl': 'events.wsdl',
+        'wsdl': 'ver10/events/wsdl/event.wsdl',
         'binding': 'EventBinding',
     },
     'pullpoint': {
         'ns': 'http://www.onvif.org/ver10/events/wsdl',
-        'wsdl': 'events.wsdl',
+        'wsdl': 'ver10/events/wsdl/event.wsdl',
         'binding': 'PullPointSubscriptionBinding',
     },
     'notification': {
         'ns': 'http://www.onvif.org/ver10/events/wsdl',
-        'wsdl': 'events.wsdl',
+        'wsdl': 'ver10/events/wsdl/event.wsdl',
         'binding': 'NotificationProducerBinding',
     },
     'subscription': {
         'ns': 'http://www.onvif.org/ver10/events/wsdl',
-        'wsdl': 'events.wsdl',
+        'wsdl': 'ver10/events/wsdl/event.wsdl',
         'binding': 'SubscriptionManagerBinding',
     },
     'analytics': {
         'ns': 'http://www.onvif.org/ver20/analytics/wsdl',
-        'wsdl': 'analytics.wsdl',
+        'wsdl': 'ver20/analytics/wsdl/analytics.wsdl',
         'binding': 'AnalyticsEngineBinding',
     },
     'recording': {
         'ns': 'http://www.onvif.org/ver10/recording/wsdl',
-        'wsdl': 'recording.wsdl',
+        'wsdl': 'ver10/recording.wsdl',
         'binding': 'RecordingBinding',
     },
     'search': {
         'ns': 'http://www.onvif.org/ver10/search/wsdl',
-        'wsdl': 'search.wsdl',
+        'wsdl': 'ver10/search.wsdl',
         'binding': 'SearchBinding',
     },
     'replay': {
         'ns': 'http://www.onvif.org/ver10/replay/wsdl',
-        'wsdl': 'replay.wsdl',
+        'wsdl': 'ver10/replay.wsdl',
         'binding': 'ReplayBinding',
     },
     'receiver': {
         'ns': 'http://www.onvif.org/ver10/receiver/wsdl',
-        'wsdl': 'receiver.wsdl',
+        'wsdl': 'ver10/receiver.wsdl',
         'binding': 'ReceiverBinding',
     },
 }
